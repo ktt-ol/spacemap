705f76fbf1fcc2fc537f65fff49ee59dc9edd355
Sebastian Reichel <sre@ring0.de>
2016-02-25 09:32:43 +0100

    add polyfill for SVGElement.getTransformToElement

---
+++ getTransformToElement.js
@@ -0,0 +1,6 @@
+// SVGElement.getTransformToElement() polyfill
+// Author: JointJS
+
+SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(elem) {
+    return elem.getScreenCTM().inverse().multiply(this.getScreenCTM());
+};
